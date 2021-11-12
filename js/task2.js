bf570ebf9455ce805288e26f88be1fc920451b35
Valkema <usagiserenity16@gmail.com>
2021-11-12 14:05:24 +0200

    task2 fixed: isNumValid, showFiboNumbers  functions

--- js/task2.js
+++ js/task2.js
@@ -2,38 +2,35 @@
 // Дано число. Написать функцию, которая выводит консоль числа фибоначчи до заданного.
 
 // функция isNumValid проверяет, является ли полученное число целым положительным
-// если проверка не выполняется - генерирует ошибку с сообщением о некорректных данных
 const isNumValid = (num) => {
-    try {
-        if (!Number.isInteger(num) || (num <= 0))
-        throw new RangeError("Incorrect data");
-        return num;
-    }catch(e){
-        return e;
-    };
+    return (Number.isInteger(num) && (num > 0));
 };
 
 // функция calcFibo для заданого положительного целого числа выводит в консоль все числа Фибоначчи, 
 // меньше заданого числа
 const calcFibo = (num) => {
     if (num === 1) {
+    // если число раво 1, выводится в консоль 0, как единственное меньшее число Чибоначчи
        console.log(0);
     } else {
+    // числам num-2 и num-1 присваиваются значения 0 и 1 соответственно
     let prePrevNum = 0;
     let prevNum = 1;
-    for (fiboNum = 1; fiboNum  < num ; fiboNum  = prePrevNum + prevNum) {
+    //сначала выводится в консоль первая пара чисел 0 и 1
+    console.log(prePrevNum);
+    console.log(prevNum);
+    //затем выводится сумма двух предыдущих чисел, пока результат суммы будет меньше заданного числа
+    for (let fiboNum = 1; fiboNum  < num ; fiboNum  = prePrevNum + prevNum) {
         console.log (fiboNum);
         prePrevNum = prevNum;
-        prevNum++;
-    }
-    }
+        prevNum = fiboNum;
+    }}
 };
 
 // showFiboNumbers итоговая функция, 
-// сначала выполняется проверка типа данных, если данные корректные - 
-//  выводит в консоль числа фибоначчи до заданного.
+// сначала выполняется проверка типа данных, если данные корректные - в консоль выводятся числа фибоначчи до заданного, 
+// если нет - в консоль выводится сообщение 'Incorrect data'
 const showFiboNumbers = (num) => {
     const validNum = isNumValid(num);
-    calcFibo(validNum);
+    validNum ? calcFibo(num) : console.log('Incorrect data');
 };
-
