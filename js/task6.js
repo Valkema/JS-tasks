fee0fd06d91b20411f11f11120537eb85c965f8b
Valkema <usagiserenity16@gmail.com>
2021-11-12 14:06:32 +0200

    task6 fixed: isNumValid,isLuckyTicket functions, added checkLucky function

--- js/task6.js
+++ js/task6.js
@@ -2,15 +2,8 @@
 //Дано число. Написать функцию решения счастливого билета (сумма первых 3х цифр равно сумме вторых 3х)
 
 // функция isNumValid проверяет, является ли полученное число целым неотрицательным
-// если проверка не выполняется - генерирует ошибку с сообщением о некорректных данных
 const isNumValid = (num) => {
-    try {
-        if (!Number.isInteger(num) || (num < 0))
-        throw new RangeError("Incorrect data");
-        return num;
-    }catch(e){
-        return e;
-    };
+    return (Number.isInteger(num) && (num >= 0));
 };
 
 // функция возвращает массив, состоящий из цифр заданного числа
@@ -20,10 +13,8 @@ const getNumArray = (num) => num.toString().split('').map(Number);
 // функция вычисляет сумму элементов массива
 const getSum = (arr) => arr.reduce((sum, elem) => sum + elem);
 
-// итоговая функция
-const isLuckyTicket = (num) => {
-    const validNum = isNumValid (num);
-    const numArray = getNumArray(validNum);
+const checkLucky = (num) => {
+    const numArray = getNumArray(num);
     //если число цифр не равно 6, выводит сообщение 'Not a ticket number!'
     //если в числе 6 цифр, проверяет "счастливый" билет
     if(numArray.length === 6) {
@@ -36,10 +27,16 @@ const isLuckyTicket = (num) => {
 
         //если сумма первых 3х чисел равна сумме вторых 3х чисел - выводится сообщение 'Lucky ticket!'
         //если равенство не выполняется выводится сообщение 'Not a lucky ticket!(('
-        return (
-            fisrtPartSum === secondPartSum ? console.log('Lucky ticket!') : console.log('Not a lucky ticket!((')
-        );
+            fisrtPartSum === secondPartSum ? console.log('Lucky ticket!') : console.log('Not a lucky ticket!((');
     }else{
         console.log('Not a ticket number!')
     }
+}
+
+// isLuckyTicket итоговая функция
+// сначала выполняется проверка числа, если данные корректные - вызывается функция checkLucky,
+// если нет - выводится в консоль сообщение 'Not a ticket number!'
+const isLuckyTicket = (num) => {
+    const validNum = isNumValid (num);
+    validNum ? checkLucky(num) : console.log('Not a ticket number!');
 };
